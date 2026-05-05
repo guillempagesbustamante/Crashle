25115e88c0b37b84b0bc0f1391d3f5ec7704292d
bernat.jardi <bernat.jardi@estudiantat.upc.edu>
2026-05-05 17:32:43 +0200

    Mapa d'accidents amb folium

--- ui_new4test.py
+++ ui_new4test.py
@@ -252,3 +252,17 @@ class Ui_MainWindow(object):
         self.refreshButton.setText(QCoreApplication.translate("MainWindow", u"Actualitzar", None))
     # retranslateUi
 
+# =========================
+# EJECUCIÓN
+# =========================
+if __name__ == "__main__":
+    import sys
+
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+
+    MainWindow.show()
+    sys.exit(app.exec())
